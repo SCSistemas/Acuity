eaff06eadb71cef507c9157863cbc82937d1cc6a
JUrbaez <jesusurbaez@gmail.com>
2016-12-20 16:32:42 -0400

    "Some Improvement in Login TC"

--- Login/Wrong Login TC.js
+++ Login/Wrong Login TC.js
@@ -1,13 +1,8 @@
 /**
- * Created by kmimos on 12-12-2016.
+ * Created by kmimos on 20-12-2016.
  */
 // spec.js
 describe('Protractor Demo App', function() {
-    it('UI3 Enabled', function() {
-        browser.get("http://trade-qa.acuityads.com:8080//login.html");
-        browser.driver.manage().window().maximize();
-        expect(browser.getTitle()).toEqual("AcuityAds | Login");
-    });
     it('Login - Wrong Password ', function() {
         element(by.name('userName')).clear().sendKeys('laxmi');
         element(by.name('password')).clear().sendKeys('sdasdasd');
@@ -28,12 +23,5 @@ describe('Protractor Demo App', function() {
         var el = element(by.className('btn btn-info btn-block aadev-submit ng-binding'));
         el.click();
         expect(browser.getTitle()).not.toEqual("AcuityAds | Programmatic Marketing Platform");
-    });
-    it('Right Login', function() {
-        element(by.name('userName')).clear().sendKeys('laxmi');
-        element(by.name('password')).clear().sendKeys('laxmi');
-        var el = element(by.className('btn btn-info btn-block aadev-submit ng-binding'));
-        el.click();
-        expect(browser.getTitle()).toEqual("AcuityAds | Programmatic Marketing Platform");
     });
 });
